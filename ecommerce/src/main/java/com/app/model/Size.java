0f5532919dd3348d2840fe5476daa65c654594e6
“Trish3012” <trishalasarnobat@gmail.com>
2023-11-07 16:49:12 +0530

    created Entity class for Address, Category, PaymentInformation, Product, Rating, Review, Size, User

---
+++ ecommerce/src/main/java/com/app/model/Size.java
@@ -0,0 +1,32 @@
+package com.app.model;
+
+public class Size {
+
+	private String name;
+	
+	private int quantity;
+
+	
+	
+	public Size() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
